5918ccce6c353385a7ba1bacbd73b9260baee90d
minjeong <ehdro418@naver.com>
2023-08-04 16:54:23 +0900

    :sparkles:Feat: 게시글 모델 생성

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="작성자")
+    title = models.CharField("제목", max_length=100)
+    content = models.TextField("내용")
+    image = models.ImageField("이미지", blank=True, null=True, upload_to="articles/%Y/%m/")
+    created_at = models.DateTimeField("작성시간", auto_now_add=True)
+    updated_at = models.DateTimeField("수정시간", auto_now=True)
+
+    def __str__(self):
+        return self.title
