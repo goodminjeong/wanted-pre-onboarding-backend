cf1724a3da3c5cf7a7229ff3a7d4c282155af89d
minjeong <ehdro418@naver.com>
2023-08-04 16:53:12 +0900

    :sparkles:Feat: 유저 모델 생성

--- users/models.py
+++ users/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("사용자 이메일은 필수입력 사항입니다.")
+
+        user = self.model(email=self.normalize_email(email))
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email=email, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField("이메일", max_length=255, unique=True)
+    password = models.CharField("비밀번호", max_length=20)
+    profile_image = models.ImageField(
+        "프로필이미지", upload_to="profile_images/", blank=True, null=True
+    )
+    created_at = models.DateTimeField("생성시간", auto_now_add=True)
+    updated_at = models.DateTimeField("수정시간", auto_now=True)
+    is_active = models.BooleanField("활성여부", default=True)
+    is_admin = models.BooleanField("관리자여부", default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
