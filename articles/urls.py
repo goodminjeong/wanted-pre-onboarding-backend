641f092a353002393814d77402717ef8e6650468
minjeong <ehdro418@naver.com>
2023-08-05 17:57:12 +0900

    :sparkles:Feat: 특정 게시글 조회 기능 추가

--- articles/urls.py
+++ articles/urls.py
@@ -5,4 +5,5 @@ app_name = "articles"
 
 urlpatterns = [
     path("", views.ArticleView.as_view(), name="article"),
+    path("<int:article_id>/", views.ArticleView.as_view(), name="article-detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -27,9 +28,21 @@ class ArticleView(APIView):
             status=status.HTTP_201_CREATED,
         )
 
-    def get(self, request):  # 게시글 목록 보기
-        articles = Article.objects.all()
-        pagination = PageNumberPagination()
-        paginated_articles = pagination.paginate_queryset(articles, request)
-        serializer = ArticleSerializer(paginated_articles, many=True)
-        return pagination.get_paginated_response(serializer.data)
+    def get(self, request, article_id=None):
+        if article_id:  # 특정 게시글 조회
+            article = get_object_or_404(Article, id=article_id)
+            serializer = ArticleSerializer(article)
+            return Response(
+                {
+                    "data": serializer.data,
+                    "status": status.HTTP_200_OK,
+                    "message": "게시글 조회 성공!",
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:  # 게시글 목록 조회
+            articles = Article.objects.all()
+            pagination = PageNumberPagination()
+            paginated_articles = pagination.paginate_queryset(articles, request)
+            serializer = ArticleSerializer(paginated_articles, many=True)
+            return pagination.get_paginated_response(serializer.data)
