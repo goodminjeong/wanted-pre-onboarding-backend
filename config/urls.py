1d0aff3b7426bd016a275ec7e5994b0ae444f58e
minjeong <ehdro418@naver.com>
2023-08-06 16:28:22 +0900

    :wrench:Setting: swagger 관련 코드 추가

--- config/urls.py
+++ config/urls.py
@@ -1,25 +1,40 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="pre-onboarding-backend API",
+        default_version="1.1.1",
+        description="원티드 프리온보딩-백엔드 과정 사전 과제입니다.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="ehdro418@naver.com"),
+        license=openapi.License(name="mit"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
+    re_path(
+        r"swagger(?P<format>\.json|\.yaml)",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        r"swagger",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        r"redoc", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc-v1"
+    ),
     path("admin/", admin.site.urls),
     path("api/articles/", include("articles.urls")),
     path("api/users/", include("users.urls")),
