1cbc392e76371cc421ab0c9fef0efabef5957440
minjeong <ehdro418@naver.com>
2023-08-06 11:31:48 +0900

    :white_check_mark:Test: 회원가입 테스트 추가

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+# ------------------------회원가입 테스트------------------------
+class SignupViewTest(APITestCase):
+    def test_signup_success(self):  # 회원가입 성공 테스트
+        url = reverse("users:signup")
+        data = {"email": "test@test.com", "password": "12345678"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_signup_fail(self):  # 회원가입 실패 테스트
+        url = reverse("users:signup")
+        data = {"email": "test", "password": "1234567"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["email"][0], "유효한 이메일 주소를 입력하십시오.")
+        self.assertEqual(
+            response.data["password"][0], "이 필드의 글자 수가  적어도 8 이상인지 확인하십시오."
+        )
